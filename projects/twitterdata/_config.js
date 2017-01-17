167c28553194c22832dc3e86ed4de7fd8963a6fe
scottyvg <svangilder@gmail.com>
2017-01-17 12:17:33 -0700

    First commit

---
+++ projects/twitterdata/_config.js
@@ -0,0 +1,9 @@
+var config = {};
+
+// twitter keys
+config.consumer_key = '***REMOVED***';
+config.consumer_secret = '***REMOVED***';
+config.access_token_key = '***REMOVED***';
+config.access_token_secret = '***REMOVED***';
+
+module.exports = config;
