785c3c8308feab34ba2ff0254da8a93298d479cb
Scott Van Gilder <svangilder@gmail.com>
2017-05-06 16:46:25 -0600

    update 3rd panel, update new profile pic, change education to work

--- projects/twitterdata/README.md
+++
@@ -1,6 +0,0 @@
-# Twitter Node Streaming Example
-### Steps:
-1. clone repo
-1. run ```npm i```
-1. rename _config_example.js to _config.js
-1. add api keys
--- projects/twitterdata/_config.js
+++
@@ -1,9 +0,0 @@
-var config = {};
-
-// twitter keys
-config.consumer_key = '***REMOVED***';
-config.consumer_secret = '***REMOVED***';
-config.access_token_key = '***REMOVED***';
-config.access_token_secret = '***REMOVED***';
-
-module.exports = config;
