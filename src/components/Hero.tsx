b78318cc68e22c0083f7a7a5302642e5769694bd
Scott Van Gilder <svangilder@gmail.com>
2025-07-08 11:23:40 -0600

    update image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ const Hero: React.FC = () => {
         <div className="flex flex-col md:flex-row items-center gap-8">
           <div className="w-32 h-32 md:w-40 md:h-40 relative rounded-full overflow-hidden">
             <Image
-              src="/images/svg.jpeg"
+              src="/images/ai_svg.jpeg"
               alt="Scott Van Gilder"
               fill
               className="object-cover"
