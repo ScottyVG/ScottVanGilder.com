67444548471022b062aafac41200b586a24ce84f
Scott Van Gilder <svangilder@gmail.com>
2025-07-09 16:20:39 -0600

    refactor blog header

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -39,7 +39,7 @@ function BlogContent() {
           Blog
         </h1>
         <p className="text-xl text-gray-600 dark:text-gray-300 max-w-2xl mx-auto">
-          Exploring Tech, AWS, GenAI, DevOps—and Life Outside the Code<br></br>(My opinions are my own)
+          Exploring Tech, AWS, GenAI, DevOps—and Life Outside the Code (My opinions are my own)
         </p>
       </div>
 
